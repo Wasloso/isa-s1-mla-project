d0eaaf0b2b9bae925d49d3ea216ff10c532271f1
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2026-03-20 21:16:56 +0100

    fix: correct backward propagation

--- src/loss/cross_entropy.py
+++ src/loss/cross_entropy.py
@@ -9,4 +9,5 @@ class CrossEntropy(Loss):
         return -self.xp.mean(self.xp.sum(y_true * self.xp.log(y_pred_clipped), axis=1))
 
     def backward(self, y_pred: Array, y_true: Array) -> Array:
-        return (y_pred - y_true) / y_true.shape[0]
+        y_pred_clipped = self.xp.clip(y_pred, 1e-7, 1 - 1e-7)
+        return -(y_true / y_pred_clipped) / y_true.shape[0]
